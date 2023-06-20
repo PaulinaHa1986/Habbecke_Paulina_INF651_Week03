5ea802752ae03087cad13be885412939988a518b
PaulinaHa1986 <p_habbecke@mail.fhsu.edu>
2023-06-20 15:36:08 +0200

    new name

---
+++ main.js
@@ -0,0 +1,17 @@
+var myNumber;
+myNumber = 5;
+var myOtherNumber;
+myOtherNumber = 5;
+var myTotal;
+myTotal = myNumber * myOtherNumber;
+var myName;
+myName = "Paulina Habbecke";
+var myCombo;
+myCombo = "myNumber + myOtherNumber";
+var myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+var myRemainder;
+var myRemainder = myNumber/5;
+var i = 2;
+var int = 4;
+var myBoolean = false;
